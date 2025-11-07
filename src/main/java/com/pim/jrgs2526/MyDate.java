ab63ddf0c6bdd294fd6c3fd20f5fdc068afe165d
APRM2425 <aririnmor@alu.edu.gva.es>
2025-11-07 15:37:58 +0100

    B.Validación en el constructor (para que pasen los tests de fechas válidas/invalidas)

--- src/main/java/com/pim/jrgs2526/MyDate.java
+++ src/main/java/com/pim/jrgs2526/MyDate.java
@@ -44,7 +44,44 @@ public class MyDate {
     public MyDate(){ this.year=2000; this.month=Months.JANUARY; this.day=1; }
 
     // ---A.3. Constructor con parámetros (de momento SOLO asigna)
+        // --- B.3. UTILIDAD
+    private static boolean isLeapYear(int y){
+        if (y % 400 == 0) return true;
+        if (y % 100 == 0) return false;
+        return y % 4 == 0;
+    }
+        // --- B.2. UTILIDAD
+    private static int daysInMonth(int y, Months m){
+        switch (m){
+            case JANUARY:
+            case MARCH:
+            case MAY:
+            case JULY:
+            case AUGUST:
+            case OCTOBER:
+            case DECEMBER:
+                return 31;
+
+            case APRIL:
+            case JUNE:
+            case SEPTEMBER:
+            case NOVEMBER:
+                return 30;
+
+            case FEBRUARY:
+                return isLeapYear(y) ? 29 : 28;
+            default:
+                throw new IllegalArgumentException(ERR_INVALID_MONTH);
+        }
+    }
     public MyDate(int day, Months month, int year){
+        // B. Validación en el constructor (para que pasen los tests de fechas válidas/invalidas)
+        // --B.1. Mejora
+        if (year < 0) throw new IllegalArgumentException(ERR_INVALID_YEAR);
+        if (month == null) throw new IllegalArgumentException(ERR_INVALID_MONTH);
+        int dim = daysInMonth(year, month);
+        if (day < 1 || day > dim) throw new IllegalArgumentException(ERR_INVALID_DATE);
+
         this.year = year;
         this.month = month;
         this.day = day;
